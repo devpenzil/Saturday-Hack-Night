ac2053627a0690d06d6d33205d894f6405ab2ea2
ajoalex012 <ajoalex012@gmail.com>
2021-09-04 18:37:43 +0530

    navbar

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from '../components/Navbar/NavBar'
+function HomePage() {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    )
+}
+
+export default HomePage
