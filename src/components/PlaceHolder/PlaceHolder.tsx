9124133d986391f24b3b808a1fc3439ed20019cf
ajoalex012 <ajoalex012@gmail.com>
2021-09-04 20:13:20 +0530

    profile

--- src/components/PlaceHolder/PlaceHolder.tsx
+++ src/components/PlaceHolder/PlaceHolder.tsx
@@ -1,14 +1,21 @@
+import axios from 'axios'
 import React, {useState} from 'react'
 import { useHistory } from 'react-router'
 import place from '../../assets/octo.gif'
 function PlaceHolder() {
+    const [message, setMessage] = useState("")
     const history = useHistory()
     const [username, setUsername] = useState("")
     const userSubmited = (e:any) => {
         e.preventDefault()
-        localStorage.setItem("guser",username)
-        history.push(`/profile?user=${username}`)
-
+        setMessage("Loading")
+        axios.get(`https://api.github.com/users/${username}`).then((Response)=>{
+            localStorage.setItem("guser",username)
+            history.push(`/profile?user=${username}`)
+        }).catch((Error)=>{
+            setMessage("No user Found")
+        })
+        //
     }
     return (
         <div className="container flex md:flex-row items-center flex-col mx-auto">
@@ -20,6 +27,10 @@ function PlaceHolder() {
                 <div className="text-7xl text-purple-600 font-bold">We are the cats</div>
                 <form onSubmit={userSubmited}>
                 <input onChange={(e)=>setUsername(e.target.value)} className="bg-white px-6 py-2 border-2 border-purple-600 mt-4 placeholder-purple-600 outline-none rounded-full mx-auto" type="text" name="userid" id="userid" placeholder="enter your github username" />
+                  <br /> 
+                  <div className="mt-2">
+                  {message}
+                  </div>
                 </form>
            </div>
         </div>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router'
 
 function Profile() {
     const [user, setUser]:any = useState()
+    const history = useHistory()
     const fetchinguserdetails = () => {
         axios.get(`https://api.github.com/users/${localStorage.getItem("guser")}`).then((Response)=>{
             setUser(Response.data)
@@ -13,19 +15,44 @@ function Profile() {
     useEffect(() => {
        fetchinguserdetails()
     }, [])
+    const backtohome = () => history.push("/")
     return (
         <div>
             {user ? 
             <div>
                 <div  className="card  m-4 p-4 shadow-lg rounded-lg bg-purple-200 mt-10">
-                    <img src={user.avatar_url} alt="avatar" className="w-32 mx-auto rounded-full" />
+                    <img src={user.avatar_url} alt="avatar" className="w-32 mx-auto rounded-full border-4 border-purple-400" />
                     <div className="mt-10">
                         <div className="text-center text-4xl font-bold">
                             {user.name}
                         </div>
                         <div className="text-center">@{user.login}</div>
-                        <div className="bg-purple-400 p-2 text-white text-center font-medium rounded-lg mt-4">
-                            {user.bio}
+                        <div className="mt-4 text-center"><i className="ri-map-pin-line"></i> {user.location}</div>
+                        <div className="w-full flex flex-wrap mt-6 text-lg">
+                            <div className="w-1/3">
+                                <div className="m-2  bg-purple-400 p-4 shadow-lg rounded-2xl text-center">
+                                     <i className="ri-user-follow-line"></i> {user.followers}
+                                </div>
+                            </div>
+                            <div className="w-1/3">
+                            <div className="m-2  bg-purple-400 p-4 shadow-lg rounded-2xl text-center">
+                            <i className="ri-user-follow-fill"></i> {user.following}
+                                </div>
+                            </div>
+                            <div className="w-1/3">
+                            <div className="m-2 bg-purple-400 p-4 shadow-lg rounded-2xl text-center">
+                            <i className="ri-git-repository-line"></i> {user.public_repos}
+                                </div>
+                            </div>
+                        </div>
+                        {user.bio ?
+                            <div className="bg-purple-400 p-2 text-white text-center font-medium rounded-lg mt-4">
+                             {user.bio}
+                            </div>
+                        : ""}
+
+                        <div onClick={backtohome} className="bg-red-400 cursor-pointer p-2 text-white text-center font-medium rounded-lg mt-4">
+                            Check Another profile
                         </div>
                     </div>
                 </div>
